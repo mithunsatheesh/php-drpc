bfa70aedceb19582b98df01ae50a142900f51cb4
Mithun S <mithun.s@bookmyshow.com>
2013-12-31 13:25:25 +0530

    Initital commit

---
+++ DRPC.php
@@ -0,0 +1,65 @@
+<?php
+
+function __autoload($classname) {    
+    
+    $filename = str_replace("\\", "/", "lib/".$classname . ".php");
+	
+	$filename =  dirname(__FILE__)."/".$filename;
+    if(file_exists( $filename )) {	
+		require_once($filename);
+    }
+    
+}
+
+include_once(dirname(__FILE__)."/lib/DistributedRPC.php");
+
+Class  DRPC {
+
+	var $hostName;
+	var $portNo;
+	var $timeout;
+	var $socket;
+	var $transport;
+	var $protocol;
+	
+	var $drpcClient;
+
+	function __construct($hostName,$portNo,$timeout) {		
+		
+		$this->hostName = $hostName;		
+		$this->portNo = $portNo;		
+		$this->timeout = $timeout;		
+		
+		try {
+		
+			$this->socket = new Thrift\Transport\TSocket($hostName, $portNo);
+			
+			if($timeout!=null) {
+				$this->socket->setSendTimeout($timeout);
+				$this->socket->setRecvTimeout($timeout);
+			}
+			
+			$this->transport = new Thrift\Transport\TFramedTransport($this->socket);
+			
+			$this->drpcClient = new DistributedRPCClient(new Thrift\Protocol\TBinaryProtocol($this->transport));
+			
+			$this->transport->open();			
+			
+		} catch (Exception $tx) {
+			
+			echo $tx->xdebug_message;
+			print 'TException: '.$tx->why. ' Error: '.$tx->getMessage() . "\n";
+		
+		}		
+		
+	}
+	
+	function execute($func,$params) {
+	
+		return $this->drpcClient->execute($func,$params);		
+		
+	}
+
+}
+
+?>
--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-php-drpc
-========
+# PHP-DRPC
+php drpc client written on top of thrift library to connect to drpc topologies running on storm.
 
-Storm DRPC client for php. Simply connect to any storm DRPC server by passing in the host and port of targer DRPC server.
+## How To Use
+
+just require the `DRPC.php` file in your project and create an instance of the drpc client like.
+
+```php
+
+include "includes/drpc/DRPC.php";
+$drpc = new DRPC("xxx.xxx.x.xx",3772,NULL);
+$result = $drpc->execute("CallFunctionName",$params);
+
+```
+
+## Still not working?
+If you still got trouble [mail me the issue](mail:mithunsatish@gmail.com)
